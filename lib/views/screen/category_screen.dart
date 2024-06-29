f8780b119ee0201548cdc41e32d5e0377b309d58
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-29 20:55:44 +0600

    category image upload to firebase

--- lib/views/screen/category_screen.dart
+++ lib/views/screen/category_screen.dart
@@ -1,14 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:shop_admin/constants.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   static const String routeName = "/CategoryScreen";
 
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-  uploadCategory(){
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  dynamic _image;
+  String? fileName;
+
+  late String categoryName;
+
+  pickImage()async{
+    FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: false, type: FileType.image);
+
+    if(result!=null){
+      setState(() {
+        _image = result.files.first.bytes;
+        fileName = result.files.first.name;
+      });
+    }
+  }
+
+  _uploadCategoryToStorge(dynamic image)async{
+    Reference ref = _storage.ref().child("CategoryImage").child(fileName!);
+    UploadTask uploadTask = ref.putData(image);
+
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+
+    return downloadUrl;
+  }
+
+  uploadCategory()async{
+    EasyLoading.show();
     if(_formkey.currentState!.validate()){
-      print("good guy");
+    String imageUrl = await _uploadCategoryToStorge(_image);
+
+    await _firestore.collection('categories').doc(fileName).set({
+      'image': imageUrl,
+      'categoryName': categoryName,
+    }).whenComplete((){
+      EasyLoading.dismiss();
+      setState(() {
+        _image = null;
+      });
+    });
     }else{
       print("bad request");
     }
@@ -46,7 +95,9 @@ class CategoryScreen extends StatelessWidget {
                           border: Border.all(color: Colors.grey.shade800),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child:  Center(
+                        child: _image != null
+                            ?Image.memory(_image, fit: BoxFit.cover,)
+                            :Center(
                           child: Text("Category"),
                         ),
                       ),
@@ -59,7 +110,7 @@ class CategoryScreen extends StatelessWidget {
                           ),
                         ),
                         onPressed: () {
-
+                      pickImage();
                         },
                         child: Text("Upload image",style: TextStyle(
                             color: Colors.white
@@ -73,6 +124,9 @@ class CategoryScreen extends StatelessWidget {
                   child: SizedBox(
                     width: 150,
                     child: TextFormField(
+                      onChanged: (value){
+                        categoryName = value;
+                      },
                       validator: (value){
                         if(value!.isEmpty){
                           return 'please Category Name must not be empty';
