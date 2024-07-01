7d9912140fed8eca9c39b1139db04a0b9292c720
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-07-01 16:18:57 +0600

    vendor title

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xff173e38);
+const titleColor = Colors.orange;
 
 extension View on num{
   Widget get height => SizedBox(height: toDouble(),);
--- lib/views/screen/vendor_screen.dart
+++ lib/views/screen/vendor_screen.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
 
 class VendorScreen extends StatelessWidget {
   static const String routeName = "/VendorScreen";
 
+  Widget _rowHeader(String text, int flex){
+    return Expanded(
+        flex: flex,
+        child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade700),
+        color:  titleColor
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(text, style: TextStyle(
+          color: Colors.white
+        ),),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        child: Container(
-          alignment: Alignment.topLeft,
-          padding: EdgeInsets.all(10),
-          child: Text(
-            "Vendor",
-            style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.w700
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Vendor",
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.w700
+              ),
             ),
           ),
-        ),
+          Row(
+            children: [
+              _rowHeader("LOGO", 1),
+              _rowHeader("BUSINESS NAME", 3),
+              _rowHeader("CITY", 2),
+              _rowHeader("STATE", 2),
+              _rowHeader("ACTION", 1),
+              _rowHeader("VIEW MORE", 1),
+            ],
+          )
+        ],
       ),
     );
   }
